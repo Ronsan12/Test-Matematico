5aa0f4c162bd3c72febfc065e1710df23fbbe704
Ronsan12 <ronsan12@outlook.com>
2021-11-30 19:29:05 -0500

    Test Matematico
    Test Matematico

---
+++ Test matematico/src/gameObjects/Constants.java
@@ -0,0 +1,44 @@
+
+package gameObjects;
+
+public class Constants {
+	
+	// frame dimensions
+	
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 600;
+	
+	
+	// Ufo properties
+	
+	public static final int NODE_RADIUS = 160;
+	
+	public static final double UFO_MASS = 60;
+	
+	public static final int UFO_MAX_VEL = 3;
+	
+	public static long UFO_FIRE_RATE = 1000;
+		
+	public static final int UFO_SCORE = 40;
+	
+	public static final String PLAYER1 = "1 JUGADOR";
+	
+	public static final String PLAYER2 = "DIFICULTAD";
+	
+	public static final String HELP = "AYUDA";
+
+	public static final String EXIT = "EXIT";
+	
+	public static final String QUESTION1 = "Pregunta 1 ¿cuánto es 34+11";
+	public static final String ANSWER1 = "45";
+	public static final String ANSWER2 = "56";
+	public static final String ANSWER3 = "67";
+	public static final String ANSWER4 = "90";
+	public static final String VOLVER = "Atras";
+	public static final String EASY = "Fácil";
+	public static final String NORMAL = "Normal";
+	public static final String HARD = "Difícil";
+
+
+	
+}
